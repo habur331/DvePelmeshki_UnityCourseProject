0cf1d34ac384c46df3bf462071838f5f2b5f33d7
Mierley <88342799+Mierley@users.noreply.github.com>
2023-10-14 14:12:39 +0300

    Fixed

--- Assets/Scripts/AimingTrainingRoom/AimingTrainingScene.cs
+++ Assets/Scripts/AimingTrainingRoom/AimingTrainingScene.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Serialization;
+using Random = UnityEngine.Random;
 
 namespace AimingTrainingRoom
 {
@@ -10,9 +12,58 @@ namespace AimingTrainingRoom
         [SerializeField] GameObject dartboardPrefab;
         private List<GameObject> dartboards;
 
-        public void LevelUp()
+        public void Start()
         {
+            level = 1;
+
+            CreateDartboard();
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player")) // Предположим, что игрок имеет тег "Player"
+            {
+                StartGame();
+            }
+        }
+        public void Update()
+        {
+            if (dartboards.Count == 0)
+                LevelUp();
+        }
+
+        private void StartGame()
+        {
+            level = 0;
             
+            CreateDartboard();
+        }
+
+        private void LevelUp()
+        {
+            level++;
+
+            CreateDartboard();
+        }
+
+        private void CreateDartboard()
+        {
+            dartboards.Clear();
+
+            int numberOfDartboardsToCreate = 3;
+
+            for (int i = 0; i < numberOfDartboardsToCreate; i++)
+            {
+                // Генерируем случайные координаты для новой мишени
+                float randomX = Random.Range(-10f, 10f); // Измените границы по X на ваши нужды
+                float randomY = Random.Range(0.5f, 5f); // Измените границы по Y на ваши нужды
+                float randomZ = Random.Range(-10f, 10f); // Измените границы по Z на ваши нужды
+
+                // Создаем новую мишень в случайной позиции и добавляем ее в список
+                Vector3 randomPosition = new Vector3(randomX, randomY, randomZ);
+                GameObject newDartboard = Instantiate(dartboardPrefab, randomPosition, Quaternion.identity);
+                dartboards.Add(newDartboard);
+            }
         }
     }
 }
--- Assets/Scripts/Infrastructure/ReactiveTarget.cs
+++ Assets/Scripts/Infrastructure/ReactiveTarget.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+namespace 
+
 public class ReactiveTarget : MonoBehaviour
 {
     public virtual void ReactToHit()
