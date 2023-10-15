0a77f26a82df3b7bcd9053aced5b5f0df78b7615
Ruslan Khairov <habur331@gmail.com>
2023-10-15 16:31:10 +0300

    smth

---
+++ Assets/Scripts/Character Control/LockCursor.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockCursor : MonoBehaviour
+{
+   private void Start()
+   {
+      Cursor.visible = false;
+      Cursor.lockState = CursorLockMode.Locked;
+   }
+}
--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -5,20 +5,27 @@ using Static_Classes;
 using UnityEditor.Search;
 using UnityEngine;
 using UnityEngine.Serialization;
+using Object = UnityEngine.Object;
 
 public class Gun : MonoBehaviour
 {
+    [Header("Parameters")]
     [SerializeField] private float damage = 10f;
     [SerializeField] private float fireRate = 15f;
+    [SerializeField] private float zoomInAim = 20;
+    private float _normalZoom;
+    
+    [Header("Effects")]
     [SerializeField] private GameObject bulletHole;
     [SerializeField] private new ParticleSystem particleSystem;
-    
-    private Camera _mainCamera;
+
+    private Camera _mainCamera = null;
     private float _nextTimeToFire = 0f;
     
     public void Start()
     {
         _mainCamera = Camera.main;
+        _normalZoom = _mainCamera!.fieldOfView;
     }
 
     public void Update()
@@ -28,6 +35,21 @@ public class Gun : MonoBehaviour
             Debug.Log("shot");
             _nextTimeToFire = Time.time + 1f / fireRate;
             Shoot();
+        }
+        
+        Aim();
+    }
+
+    private void Aim()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            _mainCamera.fieldOfView = zoomInAim;
+        }
+
+        if (Input.GetMouseButtonUp(1))
+        {
+            _mainCamera.fieldOfView = _normalZoom;
         }
     }
 
@@ -40,7 +62,8 @@ public class Gun : MonoBehaviour
         if (!Physics.Raycast(ray, out var hit)) return;
         
         Debug.Log(hit.transform.name);
-        Instantiate(bulletHole, hit.point + (hit.normal * .01f), Quaternion.FromToRotation(Vector3.up, hit.normal));
+        var mark = Instantiate(bulletHole, hit.point + (hit.normal * .01f), Quaternion.FromToRotation(Vector3.up, hit.normal));
+        mark.transform.parent = hit.transform;
         
         if (IsObjectReactiveTarget(hit.transform.gameObject, out var reactiveTarget))
         {
