5345ab4b7abc01ecff56476819980a2c4170e2ea
Ruslan Khairov <habur331@gmail.com>
2023-11-17 15:06:33 +0300

    Sound on hit
    smth

---
+++ Assets/Scripts/Bot/BotSound.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+[RequireComponent(typeof(BotHealth))]
+public class BotSound : MonoBehaviour
+{
+    [SerializeField] private AudioClip hitSound;
+    [SerializeField] private AudioClip headshotSound;
+
+    private BotHealth _botHealth;
+    private AudioSource _audioSource;
+
+    private void Start()
+    {
+        _botHealth = GetComponent<BotHealth>();
+        _audioSource = GetComponent<AudioSource>();
+
+        _botHealth.bodyPartHit.AddListener((bodyPart, _, _) => PlaySoundOnHit(bodyPart));
+    }
+
+    private void PlaySoundOnHit(BotBodyPartEnum bodyPart)
+    {
+        switch (bodyPart)
+        {
+            case BotBodyPartEnum.Head:
+            {
+                _audioSource.PlayOneShot(headshotSound);
+                break;
+            }
+
+            case BotBodyPartEnum.Chest:
+            case BotBodyPartEnum.Arm:
+            case BotBodyPartEnum.Leg:
+            default:
+            {
+                _audioSource.PlayOneShot(hitSound);
+                break;
+            }
+        }
+    }
+}
